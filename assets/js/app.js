341ae1ff3d64c80589f22b2b9faeb91e19255af4
Michael Fix <mfix@wisc.edu>
2016-08-19 01:14:00 -0500

    image dancing works. contact form created.

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('#contact').click(function(e) {
+    e.preventDefault();
+    $('.contact-form').show();
+  });
+  // $('.contact-form').click(function() {
+  //   $('.contact-form').hide();
+  // })
+})
