a5a3bfdd095555e9283fa57cfa20417b8960e756
Michael Fix <mfix@wisc.edu>
2016-08-24 18:41:32 -0500

    update header icon, fix carousel parseInt bug

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -18,24 +18,63 @@ $(function() {
       child.data('deg', pos);
       // console.log(pos, Math.cos(2* Math.PI * (pos/360)));
       setView(child, pos);
-      if (child.css('z-index') > topImage.css('z-index')) {
-        topImage = child;
-        $('.caption').html(child.data('caption'));
-      }
     });
   });
+
+  $(document).keydown(function(e) {
+    switch(e.which) {
+      case 37: // left
+        images.each(function(i, value) {
+          var child = $(value);
+          child.data('deg', child.data('deg') - 5);
+          debounce(setView(child, child.data('deg')), 250) ;
+        });
+        e.preventDefault();
+      break;
+
+      case 39: // right
+        images.each(function(i, value) {
+          var child = $(value);
+          child.data('deg', child.data('deg') + 5);
+          debounce(setView(child, child.data('deg')), 250) ;
+        });
+        e.preventDefault();
+      break;
+    } // prevent the default action (scroll / move caret)
+  });
+
+  function setView(elem, deg) {
+    var newPos = (50 - 25 * Math.cos(2* Math.PI * (deg/360))) + '%'
+    var scale = Math.sin(2* Math.PI * (deg/360)) + 1.1;
+    scale = Math.min(scale, 1.5);
+    var z = Math.floor(999 * Math.sin(2* Math.PI * (deg/360)));
+    // console.log(z);
+    elem.css({
+      display : 'block',
+      left: newPos,
+      transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
+      'z-index' : z
+    });
+    if (parseInt(elem.css('z-index')) > parseInt(topImage.css('z-index'))) {
+      topImage = elem;
+      $('.caption').html(elem.data('caption'));
+    }
+  }
+
 });
 
-function setView(elem, deg) {
-  var newPos = (50 - 25 * Math.cos(2* Math.PI * (deg/360))) + '%'
-  var scale = Math.sin(2* Math.PI * (deg/360)) + 1.1;
-  scale = Math.min(scale, 1.5);
-  var z = Math.floor(999 * Math.sin(2* Math.PI * (deg/360)));
-  // console.log(z);
-  elem.css({
-    display : 'block',
-    left: newPos,
-    transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
-    'z-index' : z
-  });
-}
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
