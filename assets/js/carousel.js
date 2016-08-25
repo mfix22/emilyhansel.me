79dbad65b89a6c35d0a03189de32de9b22ec45e8
Michael Fix <mfix@wisc.edu>
2016-08-24 19:07:02 -0500

    make /videos use a carousel too

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -10,13 +10,12 @@ $(function() {
   });
   $('.carousel').on('mousewheel', function(event) {
     event.preventDefault();
-    var children = $(this).children('img');
+    var children = $(this).children();
     var width = $(this).width();
     children.each(function(i, value) {
       var child = $(value);
       var pos = child.data('deg') + 5 * event.deltaY
       child.data('deg', pos);
-      // console.log(pos, Math.cos(2* Math.PI * (pos/360)));
       setView(child, pos);
     });
   });
