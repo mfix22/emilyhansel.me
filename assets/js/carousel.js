a8dc4d35cc0451c60359c05a630282cfdda674bb
Michael Fix <mfix@wisc.edu>
2016-08-29 19:56:33 -0500

    update carousel with keys

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,41 +2,49 @@ $(function() {
   var start = 90;
   var images = $('.carousel').children();
   var topImage = images.first();
+  var slice = 360/(images.length);
   $('.caption').html(topImage.data('caption'));
   images.each(function(i, value) {
     $(value).data('deg', start);
     setView($(this), start);
-    start += 360/(images.length);
+    start += slice;
   });
-  $('.carousel').on('mousewheel', function(event) {
-    event.preventDefault();
-    var children = $(this).children();
-    var width = $(this).width();
-    children.each(function(i, value) {
-      var child = $(value);
-      var pos = child.data('deg') + 5 * event.deltaY
-      child.data('deg', pos);
-      setView(child, pos);
-    });
-  });
+  // $(document).on('mousewheel', function(event) {
+  //   event.preventDefault();
+  //   var width = $('.carousel').width();
+  //   images.each(function(i, value) {
+  //     var child = $(value);
+  //     var pos = child.data('deg') + 5 * event.deltaY
+  //     child.data('deg', pos);
+  //     setView(child, pos);
+  //   });
+  // });
 
   $(document).keydown(function(e) {
     switch(e.which) {
       case 37: // left
         images.each(function(i, value) {
           var child = $(value);
-          child.data('deg', child.data('deg') - 5);
-          debounce(setView(child, child.data('deg')), 250) ;
+          var deg = child.data('deg');
+          child.data('deg', child.data('deg') - slice);
+          debounce(slideView(child, child.data('deg')), 500);
         });
+        topImage = topImage.prev();
+        if (topImage.length == 0) topImage = images.last();
+        $('.caption').html(topImage.data('caption'));
         e.preventDefault();
       break;
 
       case 39: // right
         images.each(function(i, value) {
           var child = $(value);
-          child.data('deg', child.data('deg') + 5);
-          debounce(setView(child, child.data('deg')), 250) ;
+          var deg = child.data('deg');
+          child.data('deg', child.data('deg') + slice);
+          debounce(slideView(child, child.data('deg')), 500) ;
         });
+        topImage = topImage.next();
+        if (topImage.length == 0) topImage = images.first();
+        $('.caption').html(topImage.data('caption'));
         e.preventDefault();
       break;
     } // prevent the default action (scroll / move caret)
@@ -54,12 +62,26 @@ $(function() {
       transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
       'z-index' : z
     });
-    if (parseInt(elem.css('z-index')) > parseInt(topImage.css('z-index'))) {
-      topImage = elem;
-      $('.caption').html(elem.data('caption'));
-    }
+    // if (parseInt(elem.css('z-index')) > parseInt(topImage.css('z-index'))) {
+    //   topImage = elem;
+    //   $('.caption').html(topImage.data('caption'));
+    // }
   }
 
+  function slideView(elem, deg) {
+    var newPos = (50 - 25 * Math.cos(2* Math.PI * (deg/360))) + '%'
+    var scale = Math.sin(2* Math.PI * (deg/360)) + 1.1;
+    scale = Math.min(scale, 1.5);
+    var z = Math.floor(999 * Math.sin(2* Math.PI * (deg/360)));
+    // console.log(z);
+    elem.css({
+      display : 'block',
+      transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
+      'z-index' : z
+    }).animate({
+      left: newPos
+    }, 300);
+  }
 });
 
 
