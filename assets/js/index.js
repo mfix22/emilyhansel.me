87a55d768c4cf12447295a3de87e51af40c24d67
Mike Fix <mrfix84@gmail.com>
2020-04-19 20:53:30 -0700

    update pages with more borderless content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,28 @@
-$(function() {
-  $('.contact').click(function(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    $('.mobile-nav').removeClass('active');
-    $('.nav-opener').toggleClass('active').children().toggleClass('ion-navicon').addClass('ion-navicon');
-    $('.contact-form').fadeIn(300);
-  });
-  $('form input:not([type="submit"]), form textarea').click(function(e) {
-    e.stopPropagation();
-    return false;
-  });
-  $('body').click(function() {
-    $('.contact-form').fadeOut(300);
-  });
+$(function () {
+  $('.contact').click(function (e) {
+    e.stopPropagation()
+    e.preventDefault()
+    $('.mobile-nav').removeClass('active')
+    $('.nav-opener')
+      .toggleClass('active')
+      .children()
+      .toggleClass('ion-navicon')
+      .addClass('ion-navicon')
+    $('.contact-form').fadeIn(300)
+  })
+  $('form input:not([type="submit"]), form textarea').click(function (e) {
+    e.stopPropagation()
+    return false
+  })
+  $('body').click(function () {
+    $('.contact-form').fadeOut(300)
+  })
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) $('.contact-form').fadeOut(200)
+  })
 
-  $('.nav-opener').click(function() {
-    $(this).toggleClass('active').children().toggleClass('ion-navicon').addClass('ion-close');
-    $('.mobile-nav').toggleClass('active');
-  });
-
-  // $('.email-send').submit(function(e) {
-  //   console.log('submitted');
-  //   $('.contact-form').fadeOut(300);
-  // });
+  $('.nav-opener').click(function () {
+    $(this).toggleClass('active').children().toggleClass('ion-navicon').addClass('ion-close')
+    $('.mobile-nav').toggleClass('active')
+  })
 })
