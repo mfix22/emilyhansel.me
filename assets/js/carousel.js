315370f64d6420d1ad9deaf362b455b930ed7438
Mike Fix <mrfix84@gmail.com>
2020-04-20 23:19:38 -0700

    new photo gallery

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,135 +1,148 @@
-$(function() {
-  var start = 90;
-  var images = $('.carousel').children();
-  var topImage = images.first();
-  var slice = 360/(images.length);
-  $('.caption').html(topImage.data('caption'));
-  images.each(function(i, value) {
-    $(value).data('deg', start);
-    setView($(this), start);
-    start += slice;
-  });
-  topImage.addClass('active');
-  // $(document).on('mousewheel', function(event) {
-  //   event.preventDefault();
-  //   var width = $('.carousel').width();
-  //   images.each(function(i, value) {
-  //     var child = $(value);
-  //     var pos = child.data('deg') + 5 * event.deltaY
-  //     child.data('deg', pos);
-  //     setView(child, pos);
-  //   });
-  // });
+$(function () {
+  $('img').click(function (e) {
+    var $this = $(this)
+    if (!$this.hasClass('active')) {
+      e.stopPropagation()
+      $this.addClass('active')
+      $('.caption').html($this.data('caption'))
+    } else {
+      $this.removeClass('active')
+      $('.caption').html('')
+    }
+  })
+  $('body').click(function (e) {
+    if ($('img.active').length) {
+      $('img.active').removeClass('active')
+      $('.caption').html('')
+    }
+  })
+})
+//   var start = 90
+//   var images = $('.carousel').children()
+//   var topImage = images.first()
+//   var slice = 360 / images.length
+// $('.caption').html(topImage.data('caption'))
+// images.each(function(i, value) {
+//   $(value).data('deg', start);
+//   setView($(this), start);
+//   start += slice;
+// });
+// topImage.addClass('active');
+// // $(document).on('mousewheel', function(event) {
+// //   event.preventDefault();
+// //   var width = $('.carousel').width();
+// //   images.each(function(i, value) {
+// //     var child = $(value);
+// //     var pos = child.data('deg') + 5 * event.deltaY
+// //     child.data('deg', pos);
+// //     setView(child, pos);
+// //   });
+// // });
 
-  $('.prev').click(function(e) {
-    e.preventDefault();
-    carouselBack();
-  });
+// $('.prev').click(function(e) {
+//   e.preventDefault();
+//   carouselBack();
+// });
 
-  $('.next').click(function(e) {
-    e.preventDefault();
-    carouselNext();
-  });
+// $('.next').click(function(e) {
+//   e.preventDefault();
+//   carouselNext();
+// });
 
-  $('img').click(function(e) {
-    var $this = $(this);
-    if ($this.hasClass('active')) $this.toggleClass('jumbo').css({
-      transform: '',
-    });
-  });
+// $(document).keydown(function (e) {
+//   switch (e.which) {
+//     case 37: // right
+//       e.preventDefault()
+//       carouselBack()
+//       break
 
+//     case 39: // left
+//       e.preventDefault()
+//       carouselNext()
+//       break
+//   } // prevent the default action (scroll / move caret)
+// })
 
-  $(document).keydown(function(e) {
-    switch(e.which) {
-      case 37: // right
-        e.preventDefault();
-        carouselBack();
-      break;
+// function setView(elem, deg) {
+//   var newPos = 50 - 25 * Math.cos(2 * Math.PI * (deg / 360)) + '%'
+//   var scale = Math.sin(2 * Math.PI * (deg / 360)) + 1.1
+//   scale = Math.min(scale, 1.5)
+//   var z = Math.floor(999 * Math.sin(2 * Math.PI * (deg / 360)))
+//   // console.log(z);
+//   elem.css({
+//     display: 'block',
+//     left: newPos,
+//     transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
+//     'z-index': z,
+//   })
+//   // if (parseInt(elem.css('z-index')) > parseInt(topImage.css('z-index'))) {
+//   //   topImage = elem;
+//   //   $('.caption').html(topImage.data('caption'));
+//   // }
+// }
 
-      case 39: // left
-        e.preventDefault();
-        carouselNext();
-      break;
+// function slideView(elem, deg) {
+//   var newPos = 50 - 25 * Math.cos(2 * Math.PI * (deg / 360)) + '%'
+//   var scale = Math.sin(2 * Math.PI * (deg / 360)) + 1.1
+//   scale = Math.min(scale, 1.5)
+//   var z = Math.floor(999 * Math.sin(2 * Math.PI * (deg / 360)))
+//   // console.log(z);
+//   elem
+//     .css({
+//       display: 'block',
+//       transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
+//       'z-index': z,
+//     })
+//     .animate(
+//       {
+//         left: newPos,
+//       },
+//       300
+//     )
+// }
 
-    } // prevent the default action (scroll / move caret)
-  });
+// function carouselBack() {
+//   images.each(function (i, value) {
+//     var child = $(value)
+//     var deg = child.data('deg')
+//     child.data('deg', child.data('deg') + slice)
+//     slideView(child, child.data('deg'))
+//   })
+//   topImage = topImage.prev()
+//   if (topImage.length == 0) topImage = images.last()
+//   $('.caption').html(topImage.data('caption'))
+//   images.removeClass('active')
+//   topImage.addClass('active')
+//   $('.jumbo').removeClass('jumbo')
+// }
 
-  function setView(elem, deg) {
-    var newPos = (50 - 25 * Math.cos(2* Math.PI * (deg/360))) + '%'
-    var scale = Math.sin(2* Math.PI * (deg/360)) + 1.1;
-    scale = Math.min(scale, 1.5);
-    var z = Math.floor(999 * Math.sin(2* Math.PI * (deg/360)));
-    // console.log(z);
-    elem.css({
-      display : 'block',
-      left: newPos,
-      transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
-      'z-index' : z
-    });
-    // if (parseInt(elem.css('z-index')) > parseInt(topImage.css('z-index'))) {
-    //   topImage = elem;
-    //   $('.caption').html(topImage.data('caption'));
-    // }
-  }
+// function carouselNext() {
+//   images.each(function (i, value) {
+//     var child = $(value)
+//     var deg = child.data('deg')
+//     child.data('deg', child.data('deg') - slice)
+//     slideView(child, child.data('deg'))
+//   })
+//   topImage = topImage.next()
+//   if (topImage.length == 0) topImage = images.first()
+//   $('.caption').html(topImage.data('caption'))
+//   images.removeClass('active')
+//   topImage.addClass('active')
+//   $('.jumbo').removeClass('jumbo')
+// }
 
-  function slideView(elem, deg) {
-    var newPos = (50 - 25 * Math.cos(2* Math.PI * (deg/360))) + '%'
-    var scale = Math.sin(2* Math.PI * (deg/360)) + 1.1;
-    scale = Math.min(scale, 1.5);
-    var z = Math.floor(999 * Math.sin(2* Math.PI * (deg/360)));
-    // console.log(z);
-    elem.css({
-      display : 'block',
-      transform: 'translate3d(-50%, -50%, 0px) scale(' + scale + ')',
-      'z-index' : z
-    }).animate({
-      left: newPos
-    }, 300);
-  }
-
-  function carouselBack() {
-    images.each(function(i, value) {
-      var child = $(value);
-      var deg = child.data('deg');
-      child.data('deg', child.data('deg') + slice);
-      slideView(child, child.data('deg'));
-    });
-    topImage = topImage.prev();
-    if (topImage.length == 0) topImage = images.last();
-    $('.caption').html(topImage.data('caption'));
-    images.removeClass('active');
-    topImage.addClass('active');
-    $('.jumbo').removeClass('jumbo');
-  }
-
-  function carouselNext() {
-    images.each(function(i, value) {
-      var child = $(value);
-      var deg = child.data('deg');
-      child.data('deg', child.data('deg') - slice);
-      slideView(child, child.data('deg'));
-    });
-    topImage = topImage.next();
-    if (topImage.length == 0) topImage = images.first();
-    $('.caption').html(topImage.data('caption'));
-    images.removeClass('active');
-    topImage.addClass('active');
-    $('.jumbo').removeClass('jumbo');
-  }
-});
-
-
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+// function debounce(func, wait, immediate) {
+//   var timeout
+//   return function () {
+//     var context = this,
+//       args = arguments
+//     var later = function () {
+//       timeout = null
+//       if (!immediate) func.apply(context, args)
+//     }
+//     var callNow = immediate && !timeout
+//     clearTimeout(timeout)
+//     timeout = setTimeout(later, wait)
+//     if (callNow) func.apply(context, args)
+//   }
+// }
