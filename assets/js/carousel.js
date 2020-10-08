691a6c900c1fb8bd5ccc1195a0f35f1b36bedf6d
Mike Fix <mrfix84@gmail.com>
2020-10-07 19:42:35 -0700

    update image re-layout

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,4 +2,18 @@ $(function () {
   $('img').click(function () {
     $('.full-screen-caption').html($(this).data('caption'))
   })
+
+  /**
+   * Relayout masonry after every image load
+   *
+   * IDEA: use imagesloaded as demonstrated:
+   *  - https://masonry.desandro.com/layout.html
+   *  - https://imagesloaded.desandro.com/
+   */
+  const grid = $('.grid')
+  grid.find('img').each(() => {
+    $(this).on('load', () => {
+      grid.masonry('layout')
+    })
+  })
 })
