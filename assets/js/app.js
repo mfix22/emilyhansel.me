7b74458365d8116bb318dfd7b0a1c900f1d61494
Mike Fix <mrfix84@gmail.com>
2022-03-12 18:22:27 -0800

    add fourbyfour/ask page and tweak stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,18 @@
-$(function() {
-  $('.menu-button').on('mouseover click', function() {
-    $('.menu-item.active').removeClass('active');
+$(function () {
+  $('.menu-item.active').removeClass('active')
+  function handler() {
+    $('.menu-item.active').removeClass('active')
     $(this).parent().addClass('active')
-  });
-});
+  }
+  $('.menu-button').on('mouseover click', handler)
+  $('.menu-button').on('focus', handler)
+})
 
 // animation does not work in Firefox
-if (!sessionStorage.seen && (navigator.userAgent.toLowerCase().indexOf('firefox') <= -1)) {
-  $('.dancing-image').addClass('dance');
-  $('.leftImage').addClass('fadeInLate');
-  $('.body-container').addClass('menu-fade-in');
+if (!sessionStorage.seen && navigator.userAgent.toLowerCase().indexOf('firefox') <= -1) {
+  $('.dancing-image').addClass('dance')
+  $('.leftImage').addClass('fadeInLate')
+  $('.body-container').addClass('menu-fade-in')
 } else {
-  delete sessionStorage.seen;
+  delete sessionStorage.seen
 }
