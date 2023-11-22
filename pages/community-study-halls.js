34ecc1e25ffc80af6f532e40e2a1503c8de5886b
Mike Fix <mrfix84@gmail.com>
2023-11-22 13:34:08 -0800

    move to .js file

---
+++ pages/community-study-halls.js
@@ -0,0 +1,320 @@
+import React from "react";
+
+import { Link } from "../components/Link";
+
+export default function CommunityStudyHalls() {
+  return (
+    <main>
+      <article>
+        <h1>Community Study Halls</h1>
+        <h2 className="subtitle">for dancers and choreographers</h2>
+
+        <p>
+          I recently got funding from the SF Arts Commission (woohoo!) to host
+          this series of six Community Study Halls that will bring dancers and
+          choreographers/directors together in discussion about cultivating
+          healthy, equitable, and ethical workplaces in our contemporary dance
+          field. The series will also function as source material for the
+          creative development of a new choreographic work that I’m creating
+          with dancer-collaborators Rebecca Fitton, Audrey Johnson, Jocelyn
+          Reyes, and Erin Yen, which will premiere May 16-18, 2024.
+        </p>
+
+        <p>
+          My goal is for the Study Halls to be a place where people who hire
+          dancers and people who are hired as dancers (or maybe you're both,
+          like me!) can speak about the equitable hiring practices and working
+          conditions they desire and how to make them a reality. We'll do some
+          dreaming and will also focus on the logistics of implementation.
+        </p>
+
+        <p>
+          I'd love to have you join us if you feel like sharing, venting,
+          brainstorming, witnessing, or otherwise participating in conversations
+          about the topics listed below. Come to all six or drop in to the ones
+          that you’re most interested in. All are welcome, and RSVP is requested
+          for each individual session you plan to attend.
+        </p>
+
+        <p>
+          If anything about this project prompts questions, feedback, or ideas,
+          I invite you to email me so we can connect and discuss further.
+        </p>
+
+        <p className="indent">
+          In solidarity,
+          <br />
+          —Emily Hansel
+        </p>
+
+        <p></p>
+
+        <div>
+          <h2>Dreaming up a dancer’s utopia</h2>
+          <p>
+            <strong>When: </strong>
+            Thursday, 12/7, 1:00-3:00pm
+            <br />
+            <strong>Where: </strong>
+            Steppin’ Out Dance Studio (
+            <Link href="https://maps.app.goo.gl/5wSawB8y9ZQ2WmQLA">
+              697 S Van Ness Ave
+            </Link>
+            )
+            <br />
+            <strong>What we’re getting at:</strong>
+            <br />
+          </p>
+          <ul>
+            <li>
+              Dancers, what do you need and want in a dance job or process?
+            </li>
+            <li>What tangible things can make our field more sustainable?</li>
+            <li>What things make you feel more supported and safe?</li>
+            <li>
+              Let’s leave behind our scarcity mindset and dream big to generate
+              a list of concrete desires.
+            </li>
+          </ul>
+
+          <div className="btn-old">
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSc7GN1rhUX2S-TPzMBAhGrA854youAdF7g0-pf43hSFV-wkSw/viewform?usp=sf_link">
+              RSVP to attend
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <h2>The emotional labor of writing about our work</h2>
+          <p>
+            <strong>When: </strong>
+            Monday, 12/11, 12:00-2:00pm
+            <br />
+            <strong>Where: </strong>
+            Steppin’ Out Dance Studio (
+            <Link href="https://maps.app.goo.gl/5wSawB8y9ZQ2WmQLA">
+              697 S Van Ness Ave
+            </Link>
+            )
+            <br />
+            <strong>What we’re getting at:</strong>
+            <br />
+          </p>
+          <ul>
+            <li>What emotions does grant writing bring up for you?</li>
+            <li>
+              How do you feel about communicating about your dance work via the
+              medium of spoken or written language?
+            </li>
+            <li>
+              How do you talk about yourself and your work in person vs. on
+              social media vs. for grant applications?
+            </li>
+            <li>
+              Feelings about representing ourselves differently in different
+              contexts
+            </li>
+          </ul>
+
+          <div className="btn-old">
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLScK7iD-Pdgx3TvGHvAfy8voleoFenVlwgzn_45uzcOBBIQONw/viewform?usp=sf_link">
+              RSVP to attend
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <h2>Ownership and credit in collaborative processes</h2>
+          <p>
+            <strong>When: </strong>
+            Friday, 12/15, 1:00-3:00pm
+            <br />
+            <strong>Where: </strong>
+            Steppin’ Out Dance Studio (
+            <Link href="https://maps.app.goo.gl/5wSawB8y9ZQ2WmQLA">
+              697 S Van Ness Ave
+            </Link>
+            )
+            <br />
+            <strong>What we’re getting at:</strong>
+            <br />
+          </p>
+          <ul>
+            <li>
+              Holding a thoughtful container for dancer-generated movement
+            </li>
+            <li>
+              How does it feel when you’re asked to generate material in
+              different contexts?
+            </li>
+            <li>
+              How can we manage expectations and form sincere and clear
+              agreements around collaboration, ownership, and credit?
+            </li>
+            <li>
+              Examples from our experiences where we felt good about the way
+              credit and ownership were being handled
+            </li>
+          </ul>
+
+          <div className="btn-old">
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeNbFWr5xB4yEp7CjY7wqB2qM8uuBbPmOMOI9E7T1_HYtJ1vw/viewform?usp=sf_link">
+              RSVP to attend
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <h2>Building budgets that prioritize dancer pay</h2>
+          <p>
+            <strong>When: </strong>
+            Saturday, 1/6, 2:00-4:00pm
+            <br />
+            <strong>Where: </strong>
+            Impact Center (
+            <Link href="https://maps.app.goo.gl/osSCaUCm2KnXWsjg8">
+              1625 Bush St
+            </Link>
+            )
+            <br />
+            <strong>What we’re getting at:</strong>
+            <br />
+          </p>
+          <ul>
+            <li>
+              Making sure our budget aligns with our values and tells a story we
+              can be proud of
+            </li>
+            <li>
+              Different pay rates for different artists/roles in a production
+            </li>
+            <li>
+              What’s an appropriate base rate/minimum wage/living wage for
+              dancers? Other benefits for dancers?
+            </li>
+            <li>How can a budget be geared toward sustainability?</li>
+            <li>Compensation for choreographers/directors</li>
+          </ul>
+
+          <div className="btn-old">
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSexq8oYTBBtSExztjOb77h3s3MerGLGfKuasW7DJ0u03uK9Fg/viewform?usp=sf_link">
+              RSVP to attend
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <h2>How can we hold decision-makers accountable?</h2>
+          <p>
+            <strong>When: </strong>
+            Sunday, 1/14, 4:30-6:30pm
+            <br />
+            <strong>Where: </strong>
+            Location TBD (San Francisco)
+            <br />
+            <strong>What we’re getting at:</strong>
+            <br />
+          </p>
+          <ul>
+            <li>
+              Fostering healthy and constructive conversations about
+              accountability with people in leadership positions
+            </li>
+            <li>
+              How do we communicate when things aren’t working (for either
+              dancer or director)?
+            </li>
+            <li>
+              How to respectfully say no, and be prepared to hear someone else
+              say no to you.
+            </li>
+            <li>
+              Conversations across power dynamics (artist, collaborators,
+              directors, funders, presenters, etc.)
+            </li>
+            <li>
+              Building in checks to power, asking for and providing sincere
+              feedback
+            </li>
+            <li>
+              Ways to articulate inclusivity without being or feeling
+              performative
+            </li>
+            <li>
+              Concrete examples from our experiences where a leader has changed
+              their practices toward equity and safety
+            </li>
+          </ul>
+
+          <div className="btn-old">
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeu5r1JU1sVwv5AE6HQ2RdAuOyACt0wnizanw7MCy2Picgwlg/viewform?usp=sf_link">
+              RSVP to attend
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <h2>Ideas for resource sharing</h2>
+          <p>
+            <strong>When: </strong>
+            Monday, 1/22, 5:30-7:30pm
+            <br />
+            <strong>Where: </strong>
+            Steppin’ Out Dance Studio (
+            <Link href="https://maps.app.goo.gl/5wSawB8y9ZQ2WmQLA">
+              697 S Van Ness Ave
+            </Link>
+            )
+            <br />
+            <strong>What we’re getting at:</strong>
+            <br />
+          </p>
+          <ul>
+            <li>How to amplify the resources we have to support more people</li>
+            <li>
+              Collectively working as an ecosystem to share resources like
+              space, time, money, and knowledge
+            </li>
+            <li>
+              Planning and coordinating to minimize conflicts and competition in
+              the project-based, freelance dance community
+            </li>
+            <li>Naming obstacles to resource sharing</li>
+            <li>Bring your own ideas!</li>
+          </ul>
+
+          <div className="btn-old">
+            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfga84ISF4ogm9aedcfYUTQ8_tRrpuBscd2J-ODyOrOU999KA/viewform?usp=sf_link">
+              RSVP to attend
+            </Link>
+          </div>
+        </div>
+      </article>
+      <style jsx>
+        {`
+          p {
+            margin: 0 0 0.5rem;
+          }
+
+          h1 {
+            margin-bottom: 0;
+          }
+
+          .subtitle {
+            font-weight: 100;
+            color: white;
+            margin: 0 0 4rem;
+          }
+
+          .indent {
+            margin-left: 4rem;
+          }
+
+          .btn-old {
+            margin-top: 2rem;
+          }
+        `}
+      </style>
+    </main>
+  );
+}
