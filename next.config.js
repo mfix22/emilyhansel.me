e5182a0a8448b94359762dfe019975fa0e6a03b9
Mike Fix <mrfix84@gmail.com>
2024-03-25 21:43:22 -0700

    set lang

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ const withMarkdoc = require("@markdoc/next.js");
 
 module.exports = withMarkdoc()({
   pageExtensions: ["js", "md", "mdoc"],
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
 });
